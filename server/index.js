420e98ea1aaec897017b16408e8bb3065a2e5162
iansh07 <152841204+iansh07@users.noreply.github.com>
2024-09-06 13:15:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "https://studynotion-frontend-gold.vercel.app/",
+		origin: "https://studynotion-frontend-gold.vercel.app",
 		credentials: true,
 	})
 );
